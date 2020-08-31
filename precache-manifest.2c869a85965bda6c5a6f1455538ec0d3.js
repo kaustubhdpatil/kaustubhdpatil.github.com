11d2ae5789214218dbb75bf46d47eea25ea6043b
kaustubhdpatil <kdpatil.19@gmail.com>
2020-08-30 23:50:56 -0400

    First draft commit

---
+++ precache-manifest.2c869a85965bda6c5a6f1455538ec0d3.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a2431903b00019d609dc8a1eefe68f",
+    "url": "/kaustubhdpatil/PersonalWebsite/index.html"
+  },
+  {
+    "revision": "cbc82a162dc739a74a78",
+    "url": "/kaustubhdpatil/PersonalWebsite/static/js/2.b70700cb.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/kaustubhdpatil/PersonalWebsite/static/js/2.b70700cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819f538a396e326d1873",
+    "url": "/kaustubhdpatil/PersonalWebsite/static/js/main.f52bc2d5.chunk.js"
+  },
+  {
+    "revision": "fbff422a3e274ef7b3e3",
+    "url": "/kaustubhdpatil/PersonalWebsite/static/js/runtime-main.1cc5d19b.js"
+  }
+]);
